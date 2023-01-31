f3a4d783c457a0c6cb2b35c8b8fa31ccf42c32c9
Matheus <giehlmatheus96@gmail.com>
2023-01-31 20:00:49 -0300

    components

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'
+
+const Header = () => {  
+    return (
+        <div>
+          <div className='header'>
+            <h1 className='name'>Matheus Giehl</h1>
+          </div>
+          <div className='line'></div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Page = () => {
+    return (
+        <div className='page'>
+            <div className='left--side'></div>
+            <div className='right--side'></div>
+        </div>
+    );
+};
+
+export default Page;
